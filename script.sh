63bf92e994871e13eab12384b4f422415a337cc7
erikasenova <erikasenova@luboe.myguest.virtualbox.org>
2024-04-19 23:09:45 +0300

    dopilila mb

--- script.sh
+++ script.sh
@@ -1,4 +1,23 @@
-find $1 -type f
-echo "----------"
-find $1 -type d
-find $1 -type f -exec cp {} $2 \;
+
+#!/bin/bash
+
+find "$1" -maxdepth 1 -type f
+
+find "$1" -maxdepth 1 -type d
+
+find "$1" -type f
+
+files_in_input_recursive=$(find "$1" -type f)
+for file in $files_in_input_recursive; do
+    filename=$(basename "$file")
+    destination="$2/$filename"
+    if [ -e "$destination" ]; then
+        counter=1
+        while [ -e "$destination" ]; do
+            filename="${filename%.*}_$counter.${filename##*.}"
+            destination="$2/$filename"
+            ((counter++))
+        done
+    fi
+    cp "$file" "$destination"
+done
